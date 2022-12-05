7c38f42230491b5eb2c278489f4cb653db3a0068
Lily Li <yanyi918@hotmail.com>
2022-12-04 20:23:46 -0800

    finished task7, but stretch goal is not finished.

---
+++ task-7/js/taskManager.js
@@ -0,0 +1,70 @@
+const createTaskHtml = (taskId, taskType, name, description, assignedTo, dueDate, status) => {
+  const html = `
+    <li class="list-group-item" data-task-id="${taskId}">
+        <div class="d-flex w-100 mt-2 justify-content-between align-items-center">
+            <span class="badge badge-info">${taskType}</span>
+            <h5>${name}</h5>
+            <span class="badge badge-danger">${status}</span>
+        </div>
+        <div class="d-flex w-100 mb-3 justify-content-between">
+            <small>Assigned to: ${assignedTo}</small>
+            <small>Due: ${dueDate}</small>
+        </div>
+        <p>${description}</p>
+        <button type="button" class="done-button btn btn-success">Mark as done</button>
+    </li>`;
+  return html;
+};
+
+class TaskManager {
+  constructor(currentId = 0) {
+    this._tasks = [];
+    this._currentId = currentId;
+  }
+
+  get tasks() {
+    return this._tasks;
+  }
+
+  get currentId() {
+    return this._currentId;
+  }
+
+  addTask(taskType, name, description, assignedTo, dueDate, status = 'TODO') {
+    this._currentId++;
+    this._tasks.push({
+      id: this.currentId,
+      taskType,
+      name,
+      description,
+      assignedTo,
+      dueDate,
+      status
+    });
+  }
+
+  render() {
+    let tasksHtmlList = [];
+    
+    this._tasks.forEach(task => {
+      const date = new Date(task.dueDate);
+      const formattedDate = date.toLocaleDateString();
+      const taskHtml = createTaskHtml(
+        task.id,
+        task.taskType, 
+        task.name, 
+        task.description, 
+        task.assignedTo, 
+        formattedDate, 
+        task.status);
+      tasksHtmlList.push(taskHtml);
+    });
+
+    const tasksHtml = tasksHtmlList.join('\n');
+    document.getElementById("tasklist").innerHTML = tasksHtml;
+  }
+
+  getTaskById(taskId) {
+    return this._tasks.find(task => task.id == taskId);
+  }
+}
