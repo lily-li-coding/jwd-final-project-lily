b52818ceb10ba9bff0fd0bb65bf17697a3279e19
Lily Li <yanyi918@hotmail.com>
2022-12-06 00:07:45 -0800

    task 10, not finished yet

---
+++ task-10/js/taskManager.js
@@ -0,0 +1,94 @@
+const createTaskHtml = (taskId, taskType, name, description, assignedTo, dueDate, status) => {
+  const html = `
+    <li class="list-group-item" data-task-id="${taskId}">
+        <div class="d-flex w-100 mt-2 justify-content-between align-items-center">
+            <span class="badge badge-info">${taskType}</span>
+            <h5>${name}</h5>
+            <span class="badge badge-danger">${status}</span>
+        </div>
+        <div class="d-flex w-100 mb-3 justify-content-between">
+            <small>Assigned to: ${assignedTo}</small>
+            <small>Due: ${dueDate}</small>
+        </div>
+        <p>${description}</p>
+        <button type="button" class="done-button btn btn-success">Mark as done</button>
+        <button type="button" class="delete-button btn btn-danger">Delete</button>
+    </li>`;
+  return html;
+};
+
+export class TaskManager {
+  constructor(currentId = 0) {
+    this._tasks = [];
+    this._currentId = currentId;
+  }
+
+  get tasks() {
+    return this._tasks;
+  }
+
+  get currentId() {
+    return this._currentId;
+  }
+
+  addTask(taskType, name, description, assignedTo, dueDate, status = 'TODO') {
+    this._currentId++;
+    this._tasks.push({
+      id: this.currentId,
+      taskType,
+      name,
+      description,
+      assignedTo,
+      dueDate,
+      status
+    });
+  }
+
+  deleteTask(taskId) {
+    this._tasks = this._tasks.filter(task => task.id !== taskId);
+  }
+
+  render() {
+    let tasksHtmlList = [];
+    
+    this._tasks.forEach(task => {
+      const date = new Date(task.dueDate);
+      const formattedDate = date.toLocaleDateString();
+      const taskHtml = createTaskHtml(
+        task.id,
+        task.taskType, 
+        task.name, 
+        task.description, 
+        task.assignedTo, 
+        formattedDate, 
+        task.status);
+      tasksHtmlList.push(taskHtml);
+    });
+
+    const tasksHtml = tasksHtmlList.join('\n');
+    document.getElementById('tasklist').innerHTML = tasksHtml;
+  }
+
+  getTaskById(taskId) {
+    return this._tasks.find(task => task.id === taskId);
+  }
+
+  save() {
+    const tasksJson = JSON.stringify(this._tasks);
+    localStorage.setItem('tasks', tasksJson);
+    const currentId = this._currentId.toString();
+    localStorage.setItem('currentId', currentId);
+  }
+
+  load () {
+    const tasksJson = localStorage.getItem('tasks');
+    if (tasksJson) {
+      this._tasks = JSON.parse(tasksJson);
+    }
+
+    const currentId = localStorage.getItem('currentId');
+    if (currentId) {
+      this._currentId = parseInt(currentId);
+    }
+  }
+}
---
+++ task-10/test/test.js
@@ -0,0 +1,32 @@
+import assert from 'assert';
+import sinon from 'sinon';
+
+// require('../js/taskManager.js').default;
+
+import { TaskManager } from '../js/taskManager.js';
+
+describe('TaskManager', function () {
+  describe('addTest', function () {
+    it('should add a new task', function () {
+      const taskManager = new TaskManager();
+      taskManager.addTask("Home Work", "Cook", "Cook dinner", "Mary", "12/13/2022");
+      assert.equal(taskManager.tasks.length, 1);
+    });
+    it('should delete an existing task', function () {
+      const taskManager = new TaskManager();
+      taskManager.addTask("Home Work", "Cook", "Cook dinner", "Mary", "12/13/2022");
+      taskManager.deleteTask(1);
+      assert.equal(taskManager.tasks.length, 0);
+    });
+    it('should return a task by its id', function () {
+      const taskManager = new TaskManager();
+      taskManager.addTask("Home Work", "Cook", "Cook dinner", "Mary", "12/13/2022");
+      const task = taskManager.getTaskById(1);
+      assert.equal(task.taskType, "Home Work");
+      assert.equal(task.name, "Cook");
+      assert.equal(task.description, "Cook dinner");
+      assert.equal(task.assignedTo, "Mary");
+      assert.equal(task.dueDate, "12/13/2022");
+    });
+  });
+});
